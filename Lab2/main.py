ba801a4d507bcf346f0c3c8d7b6654a445279bf7
TaehongK <152193774+TaehongK@users.noreply.github.com>
2024-01-30 18:47:25 -0500

    Update main.py

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,23 +1,23 @@
 x = input("Enter color 1: ")
 y = input("Enter color 2: ")
-if x == 'red':
-    if y == 'blue':
+if x.lower() == 'red':
+    if y.lower() == 'blue':
         print("purle")
-    elif y == 'yellow':
+    elif y.lower() == 'yellow':
         print("orange")
     else:
         print("Error")
-elif x == 'blue':
-    if y == 'red':
+elif x.lower() == 'blue':
+    if y.lower() == 'red':
         print("purle")
-    elif y == 'yellow':
+    elif y.lower() == 'yellow':
         print("green")
     else:
         print("Error")
-elif x == 'yellow':
-    if y == 'red':
+elif x.lower() == 'yellow':
+    if y.lower() == 'red':
         print("orange")
-    elif y == 'blue':
+    elif y.lower() == 'blue':
         print("green")
     else:
         print("Error")
