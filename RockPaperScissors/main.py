c7464288c5f524e1a37cf820c44c825cd02b6dee
Taehong Kim <152193774+TaehongK@users.noreply.github.com>
2024-02-08 10:40:11 -0500

    Update main.py

--- RockPaperScissors/main.py
+++ RockPaperScissors/main.py
@@ -1,25 +1,41 @@
 import random
-random = random.randint(1,3)
-choice = str(input("Enter rock, paper, or scissors: "))
-def choose(x,y):
-  if y.lower() == 'rock':
-    if x == 1:
-      print("You Tied")
-    elif x == 3:
-      print("You Won")
-    elif x == 2:
-      print("You Lost")
-  elif y.lower() == 'paper':
-    if x == 2:
-      print("You Tied")
-    elif x == 1:
-      print("You Won")
-    elif x == 3:
-      print("You Lost")
-  elif y.lower() == 'scissors':
-    if x == 3:
-      print("You Tied")
-    elif x == 2:
-      print("You Won")
-    elif x == 1:
-      print("You Lost")
+#1 rock
+#2 paper
+#3 Scissor
+
+def choose():
+  t=1
+  while t == 1:
+    y = str(input("Enter rock, paper, or scissors: "))
+    x = random.randint(1,3)
+    if t == 1:
+      if y.lower() == 'rock':
+        if x == 1:
+          print("You Tied")
+        elif x == 3:
+          print("You Won")
+          t+=1
+        elif x == 2:
+          print("You Lost")
+          t+=1
+      elif y.lower() == 'paper':
+        if x == 2:
+          print("You Tied")
+        elif x == 1:
+          print("You Won")
+          t+=1
+        elif x == 3:
+          print("You Lost")
+          t+=1
+      elif y.lower() == 'scissors' or y.lower() == 'scissor':
+        if x == 3:
+          print("You Tied")
+        elif x == 2:
+          print("You Won")
+          t+=1
+        elif x == 1:
+          print("You Lost")
+          t+=1
+      else:
+        print("Invalid")
+choose()
